30e85e026b1b376d7791d6a98c02ddc15c51f7c3
JueShanCoder <lysjueshan@163.com>
2021-01-18 12:01:16 +0800

    使用HikariCP实现数据库连接池；

--- pulsar-io-jdbc/src/main/kotlin/com/boxuegu/basis/pulsar/jdbc/JdbcSink.kt
+++ pulsar-io-jdbc/src/main/kotlin/com/boxuegu/basis/pulsar/jdbc/JdbcSink.kt
@@ -2,13 +2,12 @@ package com.boxuegu.basis.pulsar.jdbc
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonParser
+import com.zaxxer.hikari.HikariDataSource
 import org.apache.pulsar.functions.api.Record
 import org.apache.pulsar.io.core.Sink
 import org.apache.pulsar.io.core.SinkContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.sql.Connection
-import java.sql.DriverManager
 import java.sql.SQLException
 
 @Suppress("unused")
@@ -17,23 +16,24 @@ class JdbcSink : Sink<ByteArray> {
         val LOGGER: Logger = LoggerFactory.getLogger(JdbcSink::class.java)
     }
 
-    private lateinit var connection: Connection
+    private lateinit var dataSource: HikariDataSource
     private lateinit var sinkConfig: JdbcSinkConfig
 
     override fun open(config: MutableMap<String, Any?>, context: SinkContext) {
         sinkConfig = config.toJdbcSinkConfig().also { it.validate() }
         Class.forName(sinkConfig.driver)
-        connection = DriverManager.getConnection(sinkConfig.jdbcUrl, sinkConfig.username, sinkConfig.password).also {
-            it.autoCommit = false
+        dataSource = HikariDataSource().also {
+            it.driverClassName = sinkConfig.driver
+            it.jdbcUrl = sinkConfig.jdbcUrl
+            it.username = sinkConfig.username
+            it.password = sinkConfig.password
+            it.isAutoCommit = false
         }
         LOGGER.info("Instance {} connected to {}", context.instanceId, sinkConfig.jdbcUrl)
     }
 
     override fun close() {
-        if (!connection.autoCommit) {
-            connection.commit()
-        }
-        connection.close()
+        dataSource.close()
     }
 
     override fun write(record: Record<ByteArray>) {
@@ -44,18 +44,20 @@ class JdbcSink : Sink<ByteArray> {
             record.entity,
             record::class
         )
-        val sql = connection.buildSQL(record.target, record.action, record.entity, record.sqlMode)
-        val statement = connection.prepareStatement(sql)
-        LOGGER.info("STATEMENT: {}", sql)
-        statement.bindValue(record.target, record.action, record.entity)
-        try {
-            statement.execute()
-            connection.commit()
-            record.ack()
-        } catch (e: SQLException) {
-            LOGGER.warn("Caught SQLException when execute statement", e)
-            connection.rollback()
-            throw e
+        dataSource.connection.use { conn ->
+            val sql = conn.buildSQL(record.target, record.action, record.entity, record.sqlMode)
+            val statement = conn.prepareStatement(sql)
+            LOGGER.info("STATEMENT: {}", sql)
+            statement.bindValue(record.target, record.action, record.entity)
+            try {
+                statement.execute()
+                conn.commit()
+                record.ack()
+            } catch (e: SQLException) {
+                LOGGER.warn("Caught SQLException when execute statement", e)
+                conn.rollback()
+                throw e
+            }
         }
     }
 }
