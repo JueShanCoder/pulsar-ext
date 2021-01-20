d982680c6bb99be81836aa25008f4483a08ecec5
JueShanCoder <lysjueshan@163.com>
2021-01-20 16:39:16 +0800

    创建数据库连接时使用连接参数中的时区配置；

--- pulsar-io-jdbc/src/main/kotlin/com/boxuegu/basis/pulsar/jdbc/JdbcSink.kt
+++ pulsar-io-jdbc/src/main/kotlin/com/boxuegu/basis/pulsar/jdbc/JdbcSink.kt
@@ -28,7 +28,6 @@ class JdbcSink : Sink<ByteArray> {
             it.username = sinkConfig.username
             it.password = sinkConfig.password
             it.isAutoCommit = false
-            it.addDataSourceProperty("serverTimezone", "GMT+8")
         }
         LOGGER.info("Instance {} connected to {}", context.instanceId, sinkConfig.jdbcUrl)
     }
