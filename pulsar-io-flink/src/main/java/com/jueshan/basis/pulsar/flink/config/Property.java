36795458c200fb7dd903b3251de9762d3b5b7375
JueShanCoder <lysjueshan@163.com>
2021-04-22 15:23:42 +0800

    新增pulsar-io-flink依赖

---
+++ pulsar-io-flink/src/main/java/com/jueshan/basis/pulsar/flink/config/Property.java
@@ -0,0 +1,38 @@
+package com.jueshan.basis.pulsar.flink.config;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+@Slf4j
+public class Property {
+    private final static String CONF_NAME = "config.properties";
+
+    private static Properties contextProperties;
+
+    static {
+        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(CONF_NAME);
+        contextProperties = new Properties();
+        InputStreamReader inputStream = new InputStreamReader(in, StandardCharsets.UTF_8);
+        try {
+            contextProperties.load(inputStream);
+        } catch (IOException e) {
+            log.error(" WARNING [请求配置资源加载失败... ]");
+            e.printStackTrace();
+        }
+        log.info(" SUCCESS [请求配置资源加载成功...]");
+    }
+
+    public static String getStrValue(String key){
+        return contextProperties.getProperty(key);
+    }
+
+    public static int getIntValue(String key){
+        String strValue = getStrValue(key);
+        return Integer.parseInt(strValue);
+    }
+}
---
+++ pulsar-io-flink/src/main/java/com/jueshan/basis/pulsar/flink/task/BinLogTask.java
@@ -0,0 +1,8 @@
+package com.jueshan.basis.pulsar.flink.task;
+
+public class BinLogTask {
+
+    public static void main(String[] args) {
+
+    }
+}
